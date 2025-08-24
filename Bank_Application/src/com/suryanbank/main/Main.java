f95bb0ee7c3f802f41b76c74d197478e222524b5
Sakthi3003 <tsakthisowmya30@gmail.com>
2025-08-24 19:49:09 +0530

    initial commit

---
+++ Bank_Application/src/com/suryanbank/main/Main.java
@@ -0,0 +1,12 @@
+package com.suryanbank.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
